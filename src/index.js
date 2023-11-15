c57d976967fe143e8e03c04c9c2380cf933ef627
claudiagiselalima <143417057+claudiagiselalima@users.noreply.github.com>
2023-11-15 17:05:34 -0300

    agrego escena 2 en index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Escena0 from "./escenas/Escena0.js"
 import Escena1 from "./escenas/Escena1.js"
+import Escena2 from "./escenas/Escena2.js"
 import GameOver from "./escenas/GameOver.js"
 import Menu from "./escenas/Menu.js"
 import Win from "./escenas/Win.js"
@@ -14,6 +15,6 @@ let config = {
             debug:false
         }
     },
-    scene:[Menu,Escena0,Escena1,GameOver,Win]
+    scene:[Menu,Escena0,Escena1,Escena2,GameOver,Win]
 }
 let game = new Phaser.Game(config);
